d379491d6fab231951349d5e2fe37edce72aeb20
Zephylon <32204417@dankook.ac.kr>
2025-05-31 21:28:16 +0900

    Update urls.py

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import chatbot_view, favorites_view, delete_favorite
+from . import views
 
 urlpatterns = [
-    path('', chatbot_view, name='chatbot'),
-    path('favorites/', favorites_view, name='favorites_view'),  # GET + POST
-    path('favorites/<int:favorite_id>/', delete_favorite, name='delete_favorite'),  # DELETE
+    path("", views.chatbot_view, name="chatbot_view"),
+    path("favorites/", views.favorites_view, name="favorites_view"),
+    path("favorites/<int:favorite_id>/", views.delete_favorite, name="delete_favorite"),
+    path("chat", views.chat_api, name="chat_api"),  
 ]
